e1e5153cdb9230e81e87d4e6921539cec0b3e72f
lelis <lelis685@gmail.com>
2023-04-21 11:33:29 -0300

    feat: Associate Subnet with Route Table

--- main.tf
+++ main.tf
@@ -46,3 +46,7 @@ resource "aws_internet_gateway" "myapp-igw" {
 }
 
 
+resource "aws_route_table_association" "myapp-rtb-subnet" {
+  subnet_id      = aws_subnet.myapp-subnet-1.id
+  route_table_id = aws_route_table.myapp-route-table.id
+}
