7390fb0a14549082c71aa9d4d64e2e2692d31069
lelis <lelis685@gmail.com>
2023-04-21 14:26:42 -0300

    feat: fix ec2 instance

--- main.tf
+++ main.tf
@@ -107,10 +107,10 @@ resource "aws_instance" "myapp-server" {
   availability_zone      = var.availability_zone
 
   associate_public_ip_address = true
-  key_name                    = "test-kp"
+
+  user_data = file("entry-script.sh")
 
   tags = {
     Name = "${var.env_prefix}-server"
   }
-
 }
