aa21d59e0647f937d3697c8bfbc07736d3a33ca4
lelis <lelis685@gmail.com>
2023-04-22 16:58:32 -0300

    feat: config remote backend s3

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_version = "> 0.13"
+  backend "s3" {
+    bucket = "myapp-bucket-lelis685"
+    key    = "myapp/state.tfstate"
+    region = "us-east-1"
+  }
+}
+
 resource "aws_vpc" "myapp-vpc" {
   cidr_block = var.vpc_cidr_block
   tags = {
